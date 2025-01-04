b6112a1c3c2da46bad4d151ec20d1ab8fac2adf5
SyazanP1 <nakanaka100@outlook.jp>
2025-01-04 15:11:53 +0900

    課題007をコミット

---
+++ kadai_007/calc.js
@@ -0,0 +1,13 @@
+// 変数
+let num = 150;
+
+// 条件分岐
+if (num % 3 == 0 && num % 5 != 0) {
+  console.log('3の倍数です');
+} else if (num % 5 == 0 && num % 3 != 0) {
+  console.log('5の倍数です');
+} else if (num % 3 == 0 && num % 5 == 0) {
+  console.log('3と5の倍数です');
+} else {
+  console.log(num);
+}
